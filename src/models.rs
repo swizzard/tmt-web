f50d5404bc2024edb24ea533a21cd8329d6916a1
Sam Raker <sam.raker@gmail.com>
2024-04-26 11:53:34 -0400

    post-merge cleanup, remove python testing remnants

--- src/models.rs
+++
@@ -1,5 +0,0 @@
-pub mod invite;
-pub mod session;
-pub mod tab;
-pub mod tag;
-pub mod user;
--- src/routes.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(test)]
-mod _test_utils;
-pub mod auth;
-pub mod misc;
-pub mod tabs;
-pub mod tags;
-pub mod users;
